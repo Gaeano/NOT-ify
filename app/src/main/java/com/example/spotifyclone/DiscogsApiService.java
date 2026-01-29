3e2ed2214afad3e2d6ae529011cf738e3e73bbca
Gaeano <francesannagaeamutia@gmail.com>
2026-01-29 22:13:24 +0800

    added DiscogsApiService added DiscogsResponse for the json results added RetrofitClient for allow retrofit to handle http requests for the API requests

---
+++ app/src/main/java/com/example/spotifyclone/DiscogsApiService.java
@@ -0,0 +1,16 @@
+package com.example.spotifyclone;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface DiscogsApiService {
+    @Headers("User-Agent: SpotifyCloneApp/1.0")
+    @GET("database/search")
+    Call<DiscogsResponse> searchDatabase(
+            @Query("q") String query,
+            @Query("type") String type,
+            @Query("token") String token
+    );
+}
---
+++ app/src/main/java/com/example/spotifyclone/DiscogsResponse.java
@@ -0,0 +1,29 @@
+package com.example.spotifyclone;
+
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class DiscogsResponse {
+
+    @SerializedName("results")
+    public List<Result> results;
+
+    public static class Result{
+        @SerializedName("id")
+        public int id;
+
+        @SerializedName("title")
+        public String title;
+
+        @SerializedName("thumb")
+        public String coverImage;
+
+        @SerializedName("artists")
+        public String artist;
+
+        @SerializedName("year")
+        public String year;
+    }
+
+}
